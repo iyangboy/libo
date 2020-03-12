7529630430c01b1099f743ed1d991afccff95ab6
杨万里 <1091231787@qq.com>
2020-03-12 13:45:51 +0800

    银行编号 后台数据

--- app/Admin/Controllers/BankCodesController.php
+++ app/Admin/Controllers/BankCodesController.php
@@ -15,7 +15,7 @@ class BankCodesController extends AdminController
      *
      * @var string
      */
-    protected $title = 'App\Models\BankCode';
+    protected $title = '支持银行';
 
     /**
      * Make a grid builder.
@@ -26,7 +26,36 @@ class BankCodesController extends AdminController
     {
         $grid = new Grid(new BankCode);
 
+        $grid->column('id', 'ID');
+        $grid->column('code', '讯联机构号');
+        $grid->column('name', '名称');
+        $grid->column('on_sale', '是否使用')->display(function ($value) {
+            return $value ? '是' : '否';
+        })->label([
+            0 => 'warning',
+            1 => 'success',
+        ]);
+        $grid->column('created_at', '创建时间');
+        $grid->column('updated_at', '更新时间');
 
+        // 去掉筛选
+        $grid->disableFilter();
+        // 去掉导出
+        $grid->disableExport();
+
+        if (!\Admin::user()->can('set-bank-codes')) {
+            $grid->tools(function ($tools) {
+                // 禁用批量删除按钮
+                $tools->batch(function ($batch) {
+                    $batch->disableDelete();
+                });
+            });
+            // 去掉新建
+            $grid->disableCreateButton();
+
+            // 关闭操作
+            $grid->disableActions();
+        }
 
         return $grid;
     }
@@ -41,7 +70,12 @@ class BankCodesController extends AdminController
     {
         $show = new Show(BankCode::findOrFail($id));
 
-
+        $show->field('id', 'ID');
+        $show->field('code', '讯联机构号');
+        $show->field('name', '名称');
+        $show->field('on_sale', '是否使用');
+        $show->field('created_at', '创建时间');
+        $show->field('updated_at', '更新时间');
 
         return $show;
     }
@@ -55,7 +89,9 @@ class BankCodesController extends AdminController
     {
         $form = new Form(new BankCode);
 
-
+        $form->text('code', '讯联机构号');
+        $form->text('name', '名称');
+        $form->switch('on_sale', '是否使用')->default(1);
 
         return $form;
     }
