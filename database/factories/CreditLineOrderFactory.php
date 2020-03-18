4f8b0e5bec200dae2e2f2a5e50440bb9b6bcc771
杨万里 <1091231787@qq.com>
2020-03-18 16:25:42 +0800

    授信额度订单模型

---
+++ database/factories/CreditLineOrderFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\CreditLineOrder;
+use Faker\Generator as Faker;
+
+$factory->define(CreditLineOrder::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
