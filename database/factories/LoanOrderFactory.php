f3462f2849b817aca304e03823646cbfd0f36d38
杨万里 <1091231787@qq.com>
2020-03-20 15:09:09 +0800

    订单模型

---
+++ database/factories/LoanOrderFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\LoanOrder;
+use Faker\Generator as Faker;
+
+$factory->define(LoanOrder::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
