88a206378a3ebbf5446fc41ee3d93bb70a00454d
杨万里 <1091231787@qq.com>
2020-03-21 20:54:13 +0800

    用户数据

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -26,24 +26,25 @@ class VerificationCodesController extends Controller
 
         $phone = $captchaData['phone'];
 
-        if (!app()->environment('production')) {
-            $code = '1234';
-        } else {
-            // 生成4位随机数，左侧补0
-            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+        $code = '1234';
+        // if (!app()->environment('production')) {
+        //     $code = '1234';
+        // } else {
+        //     // 生成4位随机数，左侧补0
+        //     $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
-            try {
-                $result = $easySms->send($phone, [
-                    'template' => config('easysms.gateways.aliyun.templates.register'),
-                    'data' => [
-                        'code' => $code
-                    ],
-                ]);
-            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-                $message = $exception->getException('aliyun')->getMessage();
-                abort(500, $message ?: '短信发送异常');
-            }
-        }
+        //     try {
+        //         $result = $easySms->send($phone, [
+        //             'template' => config('easysms.gateways.aliyun.templates.register'),
+        //             'data' => [
+        //                 'code' => $code
+        //             ],
+        //         ]);
+        //     } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+        //         $message = $exception->getException('aliyun')->getMessage();
+        //         abort(500, $message ?: '短信发送异常');
+        //     }
+        // }
 
         $key = 'verificationCode_'.Str::random(15);
         $expiredAt = now()->addMinutes(5);
